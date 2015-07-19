b5bc8d87cc0e1036848993c164f38bba22c374ef
koberlag <koberlag@gmail.com>
2015-07-19 18:01:28 -0500

    refactored fizz-buzz with nfunction and integer validation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
-	var list = $(".fizzbuzz");
-	for(var i= 1; i<=100; i++){
-		if(i%3 == 0 && i%5 == 0){
-			list.append("<li>fizzbuzz</li>");
-		}
-		else if(i%3 == 0){
-			list.append("<li>fizz</li>");
-		}
-		else if (i%5 == 0){
-			list.append("<li>buzz</li>");
-		}
-		else{
-			list.append("<li>" + i + "</li>");
+	fizzBuzz(promptCount());
+});
+
+function fizzBuzz(maxCount){
+	var list = $(".fizzbuzz"),
+		maxCount = +maxCount;
+	if(maxCount % 1 == 0)
+	{
+		for(var i= 1; i<=maxCount; i++){
+			if(i%3 == 0 && i%5 == 0){
+				list.append("<li>fizzbuzz</li>");
+			}
+			else if(i%3 == 0){
+				list.append("<li>fizz</li>");
+			}
+			else if (i%5 == 0){
+				list.append("<li>buzz</li>");
+			}
+			else{
+				list.append("<li>" + i + "</li>");
+			}
 		}
 	}
-});
+	else{
+		alert("Please enter a valid number");
+		fizzBuzz(promptCount());
+	}
+}
+
+function promptCount(){
+	return prompt("Please enter the max count", "100");
+}
